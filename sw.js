51b73072fd45bcf41eba946c76198f7a9dd8b841
ufec <ufec@users.noreply.github.com>
2022-07-11 02:49:18 +0000

    fix: :bug: 修复Goole Adsense ufec/ufec-blog-source@2829a7925789a751e3483bbbf9037c572df0fbf8

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-05005e474d28dd8d9197.js"
+    "url": "webpack-runtime-78a92b4e78f5e052f85b.js"
   },
   {
     "url": "styles.c235d52a749887f7c190.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fcc77e97a55939baba46d86332ccc544"
+    "revision": "3508efeca7635de61cae1fb6122dd214"
   },
   {
     "url": "polyfill-1e9cea9e2883d319bf3f.js"
